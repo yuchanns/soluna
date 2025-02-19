536fbf72539cb308ac0eb769d820c8324cc6a98f
Cloud Wu <cloudwu@gmail.com>
2025-02-19 20:48:45 +0800

    use mat2

--- src/texquad.glsl
+++ src/texquad.glsl
@@ -4,8 +4,7 @@ layout(binding=0) uniform vs_params {
 };
 
 struct sr_mat {
-	vec2 line1;
-	vec2 line2;
+	mat2 m;
 };
 
 layout(binding=0) readonly buffer sr_lut {
@@ -21,9 +20,7 @@ out vec2 uv;
 void main() {
 	int index = int(texcoord.z * 65535.0f);
 	vec2 t = offset * 32767.0f;
-	float dx = dot(t, sr[index].line1);
-	float dy = dot(t, sr[index].line2);
-	vec2 pos = (vec2(dx, dy) + position) * framesize;
+	vec2 pos = (t * sr[index].m + position) * framesize;
 	gl_Position = vec4(pos.x - 1.0f, pos.y + 1.0f, 0, 1);
 	uv = vec2(texcoord.x, texcoord.y);
 }
