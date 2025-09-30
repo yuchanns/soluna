cd59e7782686cbf29b1ff6389b076a620fac0de7
Hanchin Hsieh <me@yuchanns.xyz>
2025-09-30 13:35:45 +0800

    feat(version): generate version at compile time

--- src/version.h
+++ src/version.h
@@ -2,6 +2,8 @@
 #define soluna_version_h
 
 #define SOLUNA_API_VERSION 1
-#define SOLUNA_HASH_VERSION "16fd5b12ef47b7cb3aba5869c83705f52ccd4645"
+#ifndef SOLUNA_HASH_VERSION
+#define SOLUNA_HASH_VERSION "dev"
+#endif
 
 #endif
